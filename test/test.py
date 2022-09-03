9e163bd60924bcb395db872044e11e7816675cc2
tibarekb <berassut@gmail.com>
2022-09-03 22:14:08 +0300

    Changes on the pipeline

---
+++ test/test.py
@@ -0,0 +1,28 @@
+import unittest
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..')))
+import pandas as pd
+from scripts.modeling import Modeler
+
+
+class TestCases(unittest.TestCase):
+    
+    df = pd.read_csv("data/AdSmartABdata.csv")
+    analyzer = Modeler(df)
+    numeric_pipeline = analyzer.generate_pipeline("numeric")
+    numeric_transformation =  analyzer.generate_transformation(numeric_pipeline,"numeric","number")
+    numerical_features = analyzer.store_features("numeric","number")
+    
+    def test_generate_pipeline(self):
+        """
+        Test that Mlscript generates a pipeline
+        """
+        self.assertTrue(self.numeric_pipeline)
+    
+    
+    def test_store_features(self):
+        """
+        - testing store features
+        """
+        
+        self.assertTrue(self.numerical_features)
